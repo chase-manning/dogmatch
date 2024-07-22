ecfe3bdf31d04c81b00f679b3f7b188dd78db8c2
Chase Manning <chase@manning.dev>
2024-07-22 12:53:38 +0100

    :tada: create app boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100dvh;
+  padding: 2rem;
+`;
+
+const App = () => {
+  return <StyledApp>meow</StyledApp>;
+};
+
+export default App;
