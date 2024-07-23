16bbffd1247c456e74fc0a7c6ff97d2d80813dcd
Chase Manning <chase@manning.dev>
2024-07-23 20:21:15 +0100

    :rocket: add header, footer and landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import styled from "styled-components";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LandingPage from "./pages/landing/LandingPage";
 
 const StyledApp = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   min-height: 100dvh;
-  padding: 2rem;
+  border: solid 1px green;
 `;
 
 const App = () => {
-  return <StyledApp>meow</StyledApp>;
+  return (
+    <StyledApp>
+      <Header />
+      <LandingPage />
+      <Footer />
+    </StyledApp>
+  );
 };
 
 export default App;
