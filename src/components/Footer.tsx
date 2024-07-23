45068ebb9cbb035fd4861316a31fffecfebd4c7e
Chase Manning <chase@manning.dev>
2024-07-23 20:20:37 +0100

    :sparkles: create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.div`
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: solid 1px yellow;
+`;
+
+const Footer = () => {
+  return <StyledFooter>meow</StyledFooter>;
+};
+
+export default Footer;
