1f23acce946a655b1433b05a299d571ae9d74f8b
Chase Manning <chase@manning.dev>
2024-08-11 22:50:14 +0100

    :rocket: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,43 @@ import styled from "styled-components";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import LandingPage from "./pages/landing/LandingPage";
+import { Outlet, Route, Routes } from "react-router-dom";
+import { BREEDS_PATH, QUIZ_PATH } from "./app/paths";
 
 const StyledApp = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   min-height: 100dvh;
-  border: solid 1px green;
 `;
+
+const Layout = () => {
+  return (
+    <StyledApp>
+      <Header />
+      <Outlet />
+      <Footer />
+    </StyledApp>
+  );
+};
 
 const App = () => {
   return (
     <StyledApp>
-      <Header />
-      <LandingPage />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LandingPage />} />
+
+          {/* TODO */}
+          <Route path={QUIZ_PATH} element={<LandingPage />} />
+
+          {/* TODO */}
+          <Route path={BREEDS_PATH} element={<LandingPage />} />
+
+          {/* TODO */}
+          <Route path="*" element={<LandingPage />} />
+        </Route>
+      </Routes>
     </StyledApp>
   );
 };
