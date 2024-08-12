245173b592ebe3f9f362ed30fb59e96d15844aed
Chase Manning <chase@manning.dev>
2024-08-12 21:39:24 +0100

    :sparkles: add dog context provider

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { store } from "./state/store";
 import App from "./App";
 import GlobalStyles from "./styles/GlobalStyles";
 import { BrowserRouter } from "react-router-dom";
+import DogContextProvider from "./components/DogContext";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -12,10 +13,12 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <GlobalStyles />
-        <App />
-      </Provider>
+      <DogContextProvider>
+        <Provider store={store}>
+          <GlobalStyles />
+          <App />
+        </Provider>
+      </DogContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
