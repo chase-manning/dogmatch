5858d81ac8f9a0ac52a8545bc18171a69c4158a5
Chase Manning <chase@manning.dev>
2024-08-12 21:36:17 +0100

    :sparkles: create dog context

---
+++ src/components/DogContext.tsx
@@ -0,0 +1,101 @@
+import { createContext, useEffect, useState } from "react";
+
+const OPEN_DOG_REGISTRY = "https://registry.dog/api/v1";
+
+export interface DogContextType {
+  dogs: DogType[];
+  loading: boolean;
+  error: string | null;
+}
+
+export const DogContext = createContext<DogContextType>({
+  dogs: [],
+  loading: true,
+  error: null,
+});
+
+export interface DogType {
+  id: string;
+  generalInformation: {
+    name: string;
+    group: string;
+    personalityTraits: string[];
+    shortDescription: string;
+    longDescription: string;
+    popularity: number;
+    height: number;
+    weight: number;
+    lifeExpectancy: number;
+  };
+  physicalCharacteristics: {
+    size: number;
+    lifespan: number;
+    salivationTendency: number;
+    coatStyle: string;
+    coatTexture: string;
+    coatLength: number;
+    doubleCoat: boolean;
+  };
+  behavioralTraits: {
+    familyAffectionLevel: number;
+    childFriendly: number;
+    dogSociability: number;
+    friendlinessToStrangers: number;
+    playfulnessLevel: number;
+    protectiveInstincts: number;
+    adaptabilityRating: number;
+    vocalizationFrequency: number;
+  };
+  careRequirements: {
+    coatSheddingLevel: number;
+    coatGroomingFrequency: number;
+    activityLevel: number;
+    mentalStimulationRequirements: number;
+    trainingEase: number;
+  };
+  images: {
+    indoors: string;
+    outdoors: string;
+    studio: string;
+  };
+}
+
+interface Response {
+  status: "success" | "error";
+  data: DogType[];
+}
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const DogContextProvider = ({ children }: Props) => {
+  const [dogs, setDogs] = useState<DogType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchDogs = async () => {
+      try {
+        const response = await fetch(OPEN_DOG_REGISTRY);
+        console.log("Fetching dogs...");
+        const data: Response = await response.json();
+        setDogs(data.data);
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDogs();
+  }, []);
+
+  return (
+    <DogContext.Provider value={{ dogs, loading, error }}>
+      {children}
+    </DogContext.Provider>
+  );
+};
+
+export default DogContextProvider;
