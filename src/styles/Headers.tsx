3ccb49156aee30b34d0b17e14a69ed2d538bcdd5
Chase Manning <chase@manning.dev>
2024-08-14 17:44:41 +0100

    :sparkles: create header styles

---
+++ src/styles/Headers.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Header3 = styled.h3`
+  font-size: 6.4rem;
+  font-weight: 650;
+`;
+
+export const Paragraph = styled.p`
+  font-size: 2.4rem;
+  font-weight: 400;
+`;
