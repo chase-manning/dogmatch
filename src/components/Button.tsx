dbd1a3df2c277ab29a1d4205891e1ad5cac7b11e
Chase Manning <chase@manning.dev>
2024-08-14 17:43:10 +0100

    :sparkles: add link and wide option to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,33 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const StyledButton = styled.button<{ $primary?: boolean }>`
+const StyledButton = styled.button<{ $primary?: boolean; $wide?: boolean }>`
   height: 6.7rem;
   border-radius: 1rem;
   font-size: 2.4rem;
-  font-weight: 650;
+  font-weight: 600;
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
-  width: 23rem;
+  width: ${(props) => (props.$wide ? "100%" : "23rem")};
+  font-family: "Jost", sans-serif;
+
+  background: ${(props) => (props.$primary ? "var(--main)" : "var(--bg)")};
+  color: ${(props) => (props.$primary ? "var(--bg)" : "var(--main)")};
+  border: solid 2px var(--main);
+`;
+
+const StyledLink = styled(Link)<{ $primary?: boolean; $wide?: boolean }>`
+  height: 6.7rem;
+  border-radius: 1rem;
+  font-size: 2.4rem;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  width: ${(props) => (props.$wide ? "100%" : "23rem")};
   font-family: "Jost", sans-serif;
 
   background: ${(props) => (props.$primary ? "var(--main)" : "var(--bg)")};
@@ -21,13 +39,37 @@ interface Props {
   children: React.ReactNode;
   primary?: boolean;
   onClick?: () => void;
+  link?: string;
+  submit?: boolean;
+  wide?: boolean;
 }
 
-const Button = ({ children, primary, onClick }: Props) => {
+const Button = ({ children, primary, onClick, link, submit, wide }: Props) => {
+  const isInternalLink = !link?.startsWith("http");
+
   return (
-    <StyledButton $primary={primary} onClick={onClick}>
-      {children}
-    </StyledButton>
+    <>
+      {submit && (
+        <StyledButton $primary={primary} type="submit" $wide={wide}>
+          {children}
+        </StyledButton>
+      )}
+      {onclick && (
+        <StyledButton $primary={primary} onClick={onClick} $wide={wide}>
+          {children}
+        </StyledButton>
+      )}
+      {link && (
+        <StyledLink
+          to={link}
+          $primary={primary}
+          target={isInternalLink ? "" : "_blank"}
+          $wide={wide}
+        >
+          {children}
+        </StyledLink>
+      )}
+    </>
   );
 };
 
