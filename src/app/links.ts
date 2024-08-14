f023f39f421a64de8aad4f0a4e8c449c08913864
Chase Manning <chase@manning.dev>
2024-08-14 17:39:47 +0100

    :sparkles: add links

---
+++ src/app/links.ts
@@ -0,0 +1,7 @@
+export const DONATE = "https://buymeacoffee.com/chase8";
+export const OPEN_DOG_REGISTRY = "https://registry.dog/";
+export const DOGMATCH_GITHUB = "https://github.com/chase-manning/dogmatch/";
+export const DOGMATCH_CONTRIBUTE = `${DOGMATCH_GITHUB}blob/main/.github/CONTRIBUTING.md`;
+export const DOGMATCH_LICENSE = `${DOGMATCH_GITHUB}blob/main/LICENSE`;
+export const API_GITHUB = "https://github.com/chase-manning/open-dog-registry/";
+export const API_CONTRIBUTE = `${API_GITHUB}blob/main/.github/CONTRIBUTING.md`;
