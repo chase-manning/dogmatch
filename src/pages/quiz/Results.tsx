b08ba0af075132e734c052caa6dd17bd4bb29dfe
Chase Manning <chase@manning.dev>
2024-08-26 09:58:38 +0100

    :rocket: add winner

--- src/pages/quiz/Results.tsx
+++ src/pages/quiz/Results.tsx
@@ -3,18 +3,29 @@ import { DogRatings } from "../../app/dog-rating";
 import useDogs from "../../app/use-dogs";
 import { useEffect, useState } from "react";
 import Loading from "../../components/Loading";
+import Winner, { Placement } from "./Winner";
 
 const StyledResult = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 2rem;
-  position: absolute;
-  top: 0;
-  left: 0;
   background: var(--bg);
-  z-index: 3;
+  padding: 5rem 0;
+  max-width: 180rem;
+`;
+
+const Header = styled.div`
+  font-size: 4.6rem;
+  font-weight: 650;
+  margin-bottom: 8rem;
+  width: 100%;
+`;
+
+const Winners = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
 `;
 
 interface Props {
@@ -31,21 +42,32 @@ const Results = ({ ratings, show }: Props) => {
     setTimeout(() => setLoading(false), 3000);
   }, [show]);
 
+  const winners = dogs
+    .sort((a, b) => ratings[b.id].percent - ratings[a.id].percent)
+    .slice(0, 10);
+
+  if (winners.length < 10) throw new Error("Not enough winners");
+
   return (
     <StyledResult>
-      {dogs
-        .sort((a, b) => ratings[b.id].percent - ratings[a.id].percent)
-        .slice(0, 10)
-        .map((dog, i) => {
-          const rating = ratings[dog.id];
-          if (!rating) throw new Error("Rating not found");
-          return (
-            <div key={dog.id}>
-              <h2>{dog.generalInformation.name}</h2>
-              <p>{`${Math.round(rating.percent * 100)}%`}</p>
-            </div>
-          );
-        })}
+      <Header>Your perfect match is...</Header>
+      <Winners>
+        <Winner
+          dog={winners[0]}
+          placement={Placement.FIRST}
+          rating={ratings[winners[0].id]}
+        />
+        <Winner
+          dog={winners[1]}
+          placement={Placement.SECOND}
+          rating={ratings[winners[1].id]}
+        />
+        <Winner
+          dog={winners[2]}
+          placement={Placement.THIRD}
+          rating={ratings[winners[2].id]}
+        />
+      </Winners>
       {loading && <Loading>Calculating your perfect match...</Loading>}
     </StyledResult>
   );
