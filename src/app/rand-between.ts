0750d15f64826c6c627fda946f06b048611ab036
Chase Manning <chase@manning.dev>
2024-09-03 20:51:37 +0100

    :bug: fix type issue

--- src/app/rand-between.ts
+++ src/app/rand-between.ts
@@ -1,6 +1,10 @@
 import getRand from "./rand";
 
-const randBetween = (min: number, max: number, seed = undefined) => {
+const randBetween = (
+  min: number,
+  max: number,
+  seed: string | undefined = undefined
+) => {
   const rand = getRand(seed);
   return rand() * (max - min) + min;
 };
