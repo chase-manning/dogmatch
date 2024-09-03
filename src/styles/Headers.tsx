b003f87129d26a22f8a2c04e5598234957dad6c9
Chase Manning <chase@manning.dev>
2024-09-03 17:51:41 +0100

    :iphone: add mobile design for headers

--- src/styles/Headers.tsx
+++ src/styles/Headers.tsx
@@ -3,9 +3,19 @@ import styled from "styled-components";
 export const Header3 = styled.h3`
   font-size: 6.4rem;
   font-weight: 650;
+
+  @media (max-width: 900px) {
+    font-size: 3.6rem;
+    font-weight: 600;
+    text-align: center;
+  }
 `;
 
 export const Paragraph = styled.p`
   font-size: 2.4rem;
   font-weight: 400;
+
+  @media (max-width: 900px) {
+    font-size: 2.2rem;
+  }
 `;
