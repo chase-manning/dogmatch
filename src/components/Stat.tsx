a967c3024b5055caebd6e2df3013db59844fba99
Chase Manning <chase@manning.dev>
2024-09-06 20:03:52 +0100

    :iphone: implement mobile designs for stat

--- src/components/Stat.tsx
+++ src/components/Stat.tsx
@@ -5,11 +5,32 @@ import { getItemMetadata } from "../app/item-metadata";
 const StyledStat = styled.div`
   display: flex;
   align-items: center;
+
+  @media (max-width: 900px) {
+    > div:last-child {
+      display: none;
+    }
+  }
 `;
 
 const Icon = styled.img`
   width: 2.2rem;
   margin-right: 1.5rem;
+
+  @media (max-width: 900px) {
+    width: 2.6rem;
+    margin-right: 1.8rem;
+  }
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
 `;
 
 const Label = styled.div`
@@ -38,8 +59,10 @@ const Stat = ({ category, trait, icon, value }: Props) => {
   return (
     <StyledStat>
       <Icon src={icon} alt={`${metadata.label} icon`} />
-      <Label>{metadata.label}:</Label>
-      <Value>{value}</Value>
+      <TextContainer>
+        <Label>{metadata.label}:</Label>
+        <Value>{value}</Value>
+      </TextContainer>
       <Tooltip>{metadata.tooltip}</Tooltip>
     </StyledStat>
   );
