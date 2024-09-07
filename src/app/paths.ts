55ea2defbba97fca2eb76b27e848dc112768f004
Chase Manning <chase@manning.dev>
2024-09-07 13:44:59 +0100

    :sparkles: create privacy policy path

--- src/app/paths.ts
+++ src/app/paths.ts
@@ -2,3 +2,4 @@ export const BREEDS_PATH = "breeds";
 export const QUIZ_PATH = "quiz";
 export const SAY_HI_PATH = "say-hi";
 export const NOT_FOUND_PATH = "not-found";
+export const PRIVACY_POLICY_PATH = "privacy-policy";
