a8ec2f8e77fdb6aea2b829487fa5053213b227bc
Chase Manning <chase@manning.dev>
2024-09-07 14:03:04 +0100

    :sparkles: add terms and conditions path

--- src/app/paths.ts
+++ src/app/paths.ts
@@ -3,3 +3,4 @@ export const QUIZ_PATH = "quiz";
 export const SAY_HI_PATH = "say-hi";
 export const NOT_FOUND_PATH = "not-found";
 export const PRIVACY_POLICY_PATH = "privacy-policy";
+export const TERMS_AND_CONDITIONS_PATH = "terms-and-conditions";
