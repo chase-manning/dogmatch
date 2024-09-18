4040a97861cadf9c0d15d12267c003553df5e29f
Chase Manning <chase@manning.dev>
2024-09-18 11:36:53 +0100

    :sparkles: add seo component

---
+++ src/components/Seo.tsx
@@ -0,0 +1,17 @@
+import { Helmet } from "react-helmet-async";
+
+interface Props {
+  title: string;
+  description: string;
+}
+
+const Seo = ({ title, description }: Props): JSX.Element => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+    </Helmet>
+  );
+};
+
+export default Seo;
