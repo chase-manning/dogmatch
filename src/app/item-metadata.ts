8d9d184e0d99fd19fbee18a1115234c4bcda4c3e
Chase Manning <chase@manning.dev>
2024-09-22 14:33:31 +0100

    :memo: update text throughout website

--- src/app/item-metadata.ts
+++ src/app/item-metadata.ts
@@ -22,18 +22,18 @@ const ITEM_METADATA: Record<string, Record<string, Metadata>> = {
     looksImportance: {
       type: ItemType.IMPORTANCE,
       label: "Looks Importance",
-      question: "Visually, how important is the look of the dog?",
+      question: "How important to you is how the dog looks?",
       min: "Looks don't matter",
       max: "Looks are very important",
       tooltip:
-        "How important the look of the dog breed is to you relative to other characteristics such as physical characteristics and behavioral traits.",
+        "How important the look of the dog is to you, relative to other characteristics (e.g. behavioral traits, care requirements etc).",
     },
     looks: {
       type: ItemType.TOURNAMENT,
       label: "Dog Looks",
       question: "Which dog do you like the look of the most?",
       tooltip:
-        "Click on the dog you like the look of the most. That dog will then go away. Continue to select the next dog that is your favourite. This process will continue until enough data is gathered to complete your profile.",
+        "Starting with your favorite, click the dog you like the look of the most. Keep clicking your next favorite until all the dogs have gone, then a new round will start.",
     },
   },
   generalInformation: {
@@ -41,13 +41,13 @@ const ITEM_METADATA: Record<string, Record<string, Metadata>> = {
       type: ItemType.STRING,
       label: "Breed",
       tooltip:
-        "The breed of the dog, which refers to the specific genetic lineage or type of dog, such as Labrador Retriever, German Shepherd, or Beagle.",
+        "Breed of the dog, which refers to the specific genetic lineage or type of dog (e.g. Labrador Retriever, German Shepherd, Beagle, etc).",
     },
     group: {
       type: ItemType.STRING,
       label: "Group",
       tooltip:
-        "The category or classification of a dog based on its purpose or characteristics, such as Terrier, Working, or Companion.",
+        "Category of dog based on its purpose or characteristics (e.g. Terrier, Working, Companion, etc).",
     },
     personalityTraits: {
       type: ItemType.LIST,
@@ -58,41 +58,39 @@ const ITEM_METADATA: Record<string, Record<string, Metadata>> = {
     shortDescription: {
       type: ItemType.STRING,
       label: "Short Description",
-      tooltip:
-        "This section provides general information about the dog, including its background, personality traits, and any other relevant details that give an overview of the dog.",
+      tooltip: "This section provides a general overview about the dog.",
     },
     longDescription: {
       type: ItemType.STRING,
       label: "Long Description",
       tooltip:
-        "This section provides general information about the dog, including its background, personality traits, and any other relevant details that give an overview of the dog.",
+        "This section provides general information about the dog, including its background, personality traits, and any other relevant details.",
     },
     popularity: {
       type: ItemType.RATING,
       label: "Popularity",
-      min: "Rare breed",
+      min: "Rare",
       max: "Very popular",
-      question: "What level of popularity do you prefer for your dog breed?",
+      question:
+        "How well-known and commonplace do you want your dog breed to be?",
       tooltip:
-        "Indicates the popularity of the breed, reflecting how well-known the breed is, how common it is as a pet, and how easily it can be acquired.",
+        "Popularity of the breed, reflecting how well-known it is, how common it is as a pet, and how easy it is to find.",
     },
     height: {
       type: ItemType.NUMBER,
       label: "Height",
-      tooltip:
-        "This is the typical height of the dog breed measured in inches, representing how tall a dog of this breed usually stands.",
+      tooltip: "Average height of the dog in inches.",
     },
     weight: {
       type: ItemType.NUMBER,
       label: "Weight",
       tooltip:
-        "The weight of the dog breed in pounds. This value helps indicate the general size and build of the breed, providing an idea of how heavy an average dog of this breed might be.",
+        "Average weight of the dog in pounds. Also indicates size and build, giving an idea of how heavy this breed is.",
     },
     lifeExpectancy: {
       type: ItemType.NUMBER,
-      label: "Lifespan",
-      tooltip:
-        "The typical number of years a dog is expected to live, on average.",
+      label: "Life Expectancy",
+      tooltip: "Average number of years a dog will live.",
     },
   },
   physicalCharacteristics: {
@@ -101,56 +99,53 @@ const ITEM_METADATA: Record<string, Record<string, Metadata>> = {
       label: "Size",
       question: "What size of dog are you looking for?",
       min: "Small",
-      max: "Large",
-      tooltip:
-        "The relative size of a dog breed compared to other breeds. It helps users understand whether a breed is generally small, medium, or large.",
+      max: "Big",
+      tooltip: "A dog's relative size compared to other breeds.",
     },
     lifespan: {
       type: ItemType.RATING,
       label: "Lifespan",
-      question: "What lifespan do you consider ideal for a dog?",
+      question: "What lifespan do you want for your dog?",
       min: "Short (7 yrs)",
       max: "Long (17 yrs)",
-      tooltip:
-        "The average length of life for the breed compared to other dogs.",
+      tooltip: "How long a dog is likely to live compared to other dogs.",
     },
     salivationTendency: {
       type: ItemType.RATING,
       label: "Salivation Tendency",
-      question: "How do you feel about a dog that drools?",
+      question: "Do you mind a dog that drools?",
       min: "Prefer dry",
       max: "Don't mind drool",
       tooltip:
-        "This indicates how likely a breed is to drool. For those who prefer a tidier environment, breeds with high salivation tendencies may not be ideal as they can leave drool on your clothes and furniture.",
+        "How likely a breed is to drool. Breeds with high salivation may leave drool on clothes and furniture.",
     },
     coatStyle: {
       type: ItemType.STRING,
       label: "Coat Style",
-      question: "What type of coat style do you prefer in a dog?",
+      question: "What type of coat style do you prefer?",
       tooltip:
-        "The typical texture and appearance of a dog's coat, such as wavy, wiry, curly, hairless, or straight.",
+        "Texture and appearance of a dog's coat (wavy, wiry, curly, hairless or straight).",
     },
     coatTexture: {
       type: ItemType.STRING,
       label: "Coat Texture",
       question: "What texture do you prefer for your dog's coat?",
-      tooltip:
-        "Describes the typical texture of a dog's coat, such as rough, silky, or smooth.",
+      tooltip: "Texture of a dog's coat (rough, silky or smooth).",
     },
     coatLength: {
       type: ItemType.RATING,
       label: "Coat Length",
-      question: "What length of coat do you prefer on a dog?",
+      question: "How long would you prefer the dog's coat to be?",
       min: "Short",
       max: "Long",
       tooltip:
-        "The length of a dog's fur from the skin to the tip. It affects the dog's overall appearance and can influence grooming needs and seasonal care.",
+        "Length of a dog's fur from skin to tip. It affects the dog's overall appearance and can influence grooming needs and seasonal care.",
     },
     doubleCoat: {
       type: ItemType.BOOLEAN,
       label: "Double Coat",
       tooltip:
-        "Double-coated dogs have two layers of fur: a harsh topcoat and a soft undercoat. The soft undercoat is shorter, grows faster, and sheds twice a year, while the topcoat is longer and more protective.",
+        "Double-coated dogs have two layers of fur: a harsh topcoat and a soft undercoat. The soft undercoat is shorter, grows faster, and sheds twice a year. The topcoat is longer and more protective.",
     },
   },
   behavioralTraits: {
@@ -162,7 +157,7 @@ const ITEM_METADATA: Record<string, Record<string, Metadata>> = {
       min: "Reserved",
       max: "Very Affectionate",
       tooltip:
-        "Indicates how affectionate a breed is likely to be with family members or other familiar people. Some breeds may show affection primarily to their owner, while others are friendly and affectionate with everyone they know well.",
+        "How affectionate a breed is with family members or other familiar people. Some breeds may show affection primarily to their owner, while others are friendly and affectionate with everyone they know well.",
     },
     childFriendly: {
       type: ItemType.RATING,
@@ -171,25 +166,25 @@ const ITEM_METADATA: Record<string, Record<string, Metadata>> = {
       min: "Not child-friendly",
       max: "Very child-friendly",
       tooltip:
-        "This rating indicates a breed's tolerance and patience with young children's behavior and its overall family-friendly nature. Always supervise interactions between dogs and young children, or any children who are not familiar with dogs.",
+        "How tolerant and patient a dog is with young children and its overall family-friendly nature.",
     },
     dogSociability: {
       type: ItemType.RATING,
       label: "Dog Sociability",
-      question: "How sociable should your dog be with other dogs?",
+      question: "How sociable do you want your dog to be with other dogs?",
       min: "Not Sociable",
       max: "Very Sociable",
       tooltip:
-        "Indicates how friendly and social a breed typically is with other dogs. While supervision is always recommended during interactions and introductions, some breeds naturally tend to get along well with other dogs, whether at home or in public settings.",
+        "How friendly and social a breed is with other dogs. Some breeds naturally tend to get along well with other dogs.",
     },
     friendlinessToStrangers: {
       type: ItemType.RATING,
       label: "Friendliness to Strangers",
       question: "How would you like your dog to react to strangers?",
-      min: "Prefer reserved or cautious",
-      max: "Prefer very friendly and welcoming",
+      min: "Reserved or cautious",
+      max: "Very friendly",
       tooltip:
-        "This metric indicates how welcoming a breed is likely to be towards strangers. Some breeds may be reserved or cautious around new people, while others will be eager and happy to meet someone new, regardless of the location.",
+        "How welcoming a breed is towards strangers. Some breeds may be reserved or cautious around new people, while others will be eager and happy to meet someone new.",
     },
     playfulnessLevel: {
       type: ItemType.RATING,
@@ -198,7 +193,7 @@ const ITEM_METADATA: Record<string, Record<string, Metadata>> = {
       min: "Relaxed",
       max: "Very playful",
       tooltip:
-        "Indicates how enthusiastic a breed is about play, even beyond puppyhood. Some breeds remain eager to engage in activities like tug-of-war or fetch well into their adult years, while others may prefer to relax more often.",
+        "How enthusiastic a breed is about playing. Some breeds are eager to play tug-of-war or fetch well beyond puppyhood, while others prefer to relax more often.",
     },
     protectiveInstincts: {
       type: ItemType.RATING,
@@ -207,25 +202,25 @@ const ITEM_METADATA: Record<string, Record<string, Metadata>> = {
       min: "Less alert",
       max: "Highly vigilant",
       tooltip:
-        "This measures a breed's tendency to alert you to the presence of strangers or potential threats. Breeds with strong protective instincts are more likely to react to unfamiliar people or animals, whether it's the mailman or a squirrel outside. They typically become friendly towards strangers who are welcomed by their family.",
+        "How likely a breed is to warn you of strangers or potential threats. Strongly protective breeds are more likely to react to unfamiliar people or animals. They are usually more friendly towards strangers who are welcomed by their family.",
     },
     adaptabilityRating: {
       type: ItemType.RATING,
-      label: "Adaptability Rating",
+      label: "Adaptability",
       question: "How adaptable should your dog be to changes?",
-      min: "Poorly adaptable",
-      max: "Highly adaptable",
+      min: "Not very adaptable",
+      max: "Very adaptable",
       tooltip:
-        "This rating indicates how easily a breed adapts to changes in their environment, including living conditions, noise levels, weather, daily schedules, and other day-to-day variations.",
+        "How easily a breed adapts to changes in their environment (e.g. living conditions, noise levels, weather, daily schedules, etc).",
     },
     vocalizationFrequency: {
       type: ItemType.RATING,
-      label: "Vocalization Frequency",
+      label: "Barking frequency",
       question: "How do you feel about a vocal dog?",
       min: "Prefer quiet",
-      max: "It's fine if they're noisy",
+      max: "Don't mind noisy",
       tooltip:
-        "This measures how frequently a breed vocalizes, including barking and howling. While some breeds are highly vocal and bark at almost anything, others are more reserved and only bark in specific situations. Note that even barkless breeds may use other sounds to express themselves.",
+        "How frequently a breed may bark or howl. While some breeds bark at almost anything, others might only bark in specific situations (but even barkless breeds may use other sounds to express themselves.",
     },
   },
   careRequirements: {
@@ -236,25 +231,25 @@ const ITEM_METADATA: Record<string, Record<string, Metadata>> = {
       min: "Low shedding",
       max: "High shedding",
       tooltip:
-        "Indicates the amount of fur and hair the breed typically sheds. Higher shedding levels mean more frequent brushing is needed, higher chances of triggering allergies, and more frequent cleaning tasks like vacuuming and lint-rolling.",
+        "Indicates how much fur and hair the breed sheds. Higher shedding means more brushing is needed, higher chances of triggering allergies, and more frequent vacuuming.",
     },
     coatGroomingFrequency: {
       type: ItemType.RATING,
       label: "Coat Grooming Frequency",
-      question: "How frequently are you willing to groom your dog?",
+      question: "How often are you willing to groom your dog?",
       min: "Rarely",
-      max: "Frequent grooming",
+      max: "Very often",
       tooltip:
-        "This indicates how often a breed needs bathing, brushing, trimming, or other types of coat maintenance. Consider your available time, patience, and budget for grooming when evaluating this aspect. Note that all breeds need regular nail trimming.",
+        "How often a breed needs bathing, brushing, trimming or other coat maintenance, depending on your time, patience, and budget for grooming. (Note that all breeds need regular nail trimming.)",
     },
     activityLevel: {
       type: ItemType.RATING,
       label: "Activity Level",
-      question: "What level of activity do you expect from your dog?",
+      question: "How active do you want your dog to be?",
       min: "Couch potato",
       max: "Energetic explorer",
       tooltip:
-        "Indicates the required amount of physical exercise and mental stimulation for a dog breed. High activity level dogs are lively, always ready for their next adventure, and need plenty of playtime. Low activity level dogs are more laid-back and content with minimal activity.",
+        "How much physical exercise a dog needs. Very active dogs are lively, always ready for their next adventure, and need plenty of playtime. Inactive dogs are more laid-back and happy with minimal activity.",
     },
     mentalStimulationRequirements: {
       type: ItemType.RATING,
@@ -263,7 +258,7 @@ const ITEM_METADATA: Record<string, Record<string, Metadata>> = {
       min: "Low",
       max: "High",
       tooltip:
-        "Indicates how much mental stimulation a breed needs to stay happy and healthy. Breeds with high mental stimulation needs may require activities that involve decision-making, problem-solving, and concentration. Without sufficient mental engagement, these dogs might create their own activities, which may be undesirable.",
+        "How much mental stimulation a breed needs to stay happy and healthy. Breeds with high mental stimulation needs may need activities that involve decision-making, problem-solving and concentration. (Otherwise, these dogs might create their own less desirable activities!)",
     },
     trainingEase: {
       type: ItemType.RATING,
@@ -272,7 +267,7 @@ const ITEM_METADATA: Record<string, Record<string, Metadata>> = {
       min: "Challenging to train",
       max: "Easy to train",
       tooltip:
-        "Indicates how easy it is to train your dog and their willingness to learn new commands and behaviors. Some breeds are eager to please and quick to learn, while others may be more independent and stubborn.",
+        "How easy it is to train your dog and their willingness to learn new commands and behaviors. Some breeds are eager to please and quick to learn, while others may be more independent and stubborn.",
     },
   },
 };
